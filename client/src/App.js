30c3965df3d7f7cacc94cb74c8e9a8718cf1a0c3
Kendall Malik Howard <kendallhoward04@gmail.com>
2023-09-17 13:40:53 -0700

    added some login logic

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { fetchNFLTeams } from "./sportApi";
-import "./App.css"; // Import the CSS file
+import { Button, Typography, List } from "antd";
+import { LoadingOutlined, ExclamationCircleOutlined } from "@ant-design/icons";
+import { fetchNFLTeams } from "./sportApi"; // Import your data fetching function
+import "./App.css"; // Import the App.css stylesheet
 
-function SportsData() {
+function App() {
   const [teams, setTeams] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
@@ -28,14 +30,13 @@ function SportsData() {
           const responseData = await response.data;
           console.log("API Response Data:", responseData); // Log the entire response
           if (Array.isArray(responseData.results)) {
-          setTeams(responseData.results); // Use the "results" array as teams
-          setLoading(false);
-        } else {
-          console.error("Results data is not an array:", responseData.results);
-          setError("Results data is not an array");
-          setLoading(false);
-        }
-
+            setTeams(responseData.results); // Use the "results" array as teams
+            setLoading(false);
+          } else {
+            console.error("Results data is not an array:", responseData.results);
+            setError("Results data is not an array");
+            setLoading(false);
+          }
         }
       } catch (error) {
         console.error("Error fetching data:", error);
@@ -45,27 +46,35 @@ function SportsData() {
     };
 
     fetchData();
-  }, [retryCount]); // Retry whenever retryCount changes
-
-  console.log("Hello",teams)
+  }, [retryCount]);
 
   return (
-    <div>
-      <h1>NFL Teams</h1>
-      {loading ? (
-        <p>Loading...</p>
-      ) : error ? (
-        <p>Error fetching data. Please try again later.</p>
-      ) : (
-        <ul>
-          {teams.map((team, index) => (
-  <li key={index}>{team.team}</li>
-))}
-
-        </ul>
-      )}
+    <div className="app-container">
+      <div className={`app-content`}>
+        <Typography.Title level={2} className="pop-out-text">
+          NFL Teams
+        </Typography.Title>
+        {loading ? (
+          <p style={{ color: "#fff" }}>
+            Loading... <LoadingOutlined />
+          </p>
+        ) : error ? (
+          <p style={{ color: "#fff" }}>
+            Error fetching data. Please try again later. <ExclamationCircleOutlined />
+          </p>
+        ) : (
+          <List
+            dataSource={teams}
+            renderItem={(team) => (
+              <List.Item>
+                <Button type="link">{team.team}</Button>
+              </List.Item>
+            )}
+          />
+        )}
+      </div>
     </div>
   );
 }
 
-export default SportsData;
+export default App;
